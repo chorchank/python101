ab5d338bbf7473765cea0e39aebb049c80393148
RavitK <62745789+chorchank@users.noreply.github.com>
2023-02-21 14:41:43 +0700

    EP - GUI code

---
+++ GUI-Knowledge-x.py
@@ -0,0 +1,11 @@
+from tkinter import *
+GUI = Tk()
+GUI.tile('โปรแกรมบันทึกความรู้')
+GUI.geometry('500x400')
+
+
+#E1 = Entry(GUI)   #นำช่องกรอกไปใส่ในโปรแกรมหลัก
+#E1.pack()           #นำช่องกรอกไปแปะติดกับโปรแกรมหลัก
+
+GUI.mainloop()
+
